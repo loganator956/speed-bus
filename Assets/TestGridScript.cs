5669f359b6b74330e807a0b6f2831df9e2214df1
loganator956 <65763483+loganator956@users.noreply.github.com>
2021-12-10 19:20:57 +0000

    Created a grid that moves with the camera to seem infinite

---
+++ Assets/TestGridScript.cs
@@ -0,0 +1,27 @@
+using System.Runtime.CompilerServices;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGridScript : MonoBehaviour
+{
+
+    private Transform _camTransform;
+
+    private void Awake()
+    {
+        _camTransform = Camera.main.transform;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 camPos = _camTransform.position;
+        transform.position = new Vector3(Mathf.Round(camPos.x), Mathf.Round(camPos.y), 10f);
+    }
+}
