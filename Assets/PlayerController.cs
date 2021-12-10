34dcbaf11636d26b2eddacde47f9f7ae0d557db2
loganator956 <65763483+loganator956@users.noreply.github.com>
2021-12-10 20:53:13 +0000

    Started player controller, will change this

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public AnimationCurve SpeedCurve;
+    public float SpeedCurveRate = 0.5f;
+    public float MaxSpeed = 4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private Vector2 _inputs = new Vector2(0, 0);
+
+    private float _speedCurveT = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isMoving = false;
+        Vector2 newInputs = Vector2.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            isMoving = true;
+            newInputs += Vector2.up;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            isMoving = true;
+            newInputs += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            isMoving = true;
+            newInputs += Vector2.left;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            isMoving = true;
+            newInputs += Vector2.right;
+        }
+        if (isMoving)
+        {
+            _speedCurveT += Time.deltaTime * SpeedCurveRate;
+            _inputs = newInputs;
+        }
+        else if (isMoving)
+        {
+            _speedCurveT += Time.deltaTime * SpeedCurveRate * -1f;
+        }
+        _speedCurveT = Mathf.Clamp01(_speedCurveT);
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += new Vector3(_inputs.x * Time.fixedDeltaTime * SpeedCurve.Evaluate(_speedCurveT), 0, _inputs.y * Time.fixedDeltaTime * SpeedCurve.Evaluate(_speedCurveT));
+    }
+}
