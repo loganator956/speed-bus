7a293ed71f4c5531613f41f458e31fd0a807ed6e
Logan King <65763483+loganator956@users.noreply.github.com>
2022-06-25 15:58:17 +0100

    add bus stop selection method and exception

---
+++ Assets/Scripts/BusStopNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SpeedBus.Gameplay
+{
+    [Serializable]
+    public class BusStopNotFoundException : Exception
+    {
+        public BusStopNotFoundException() { }
+        public BusStopNotFoundException(string message) : base(message) { }
+        public BusStopNotFoundException(string message, Exception inner) : base(message, inner) { }
+        protected BusStopNotFoundException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,54 +5,89 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Events;
 
-public class GameController : MonoBehaviour
+namespace SpeedBus.Gameplay
 {
-    public BusStop[] busStops { get; private set; }
-    
-    private float _totalWeighting = 0;
-
-    private void Awake()
+    public class GameController : MonoBehaviour
     {
-        // find all the bus stops
-        busStops = FindObjectsOfType<BusStop>();
-        
-        foreach(BusStop stop in busStops)
+        public BusStop[] busStops { get; private set; }
+
+        private float _totalWeighting = 0;
+        public static System.Random Randomiser;
+
+        private void Awake()
         {
-            _totalWeighting += stop.Weighting;
-        }
-    }
+            Randomiser = new System.Random();
+            // find all the bus stops
+            busStops = FindObjectsOfType<BusStop>();
 
-    private void Start()
-    {
-        StartGame();
-    }
-
-    List<Person> people = new List<Person>();
-
-    public int GetTotalPeople()
-    {
-        return people.Count;
-    }
-
-    private void StartGame()
-    {
-        // TODO: Change this to be dynamically set
-        float NumberOfPassengersThisRound = 10;
-        foreach(BusStop busStop in busStops)
-        {
-            people.AddRange(busStop.GeneratePeople(Mathf.CeilToInt((busStop.Weighting / _totalWeighting) * NumberOfPassengersThisRound)));
-        }
-        System.Random random = new System.Random();
-        foreach(Person person in people)
-        {
-            while(person.WantedStop == null)
+            foreach (BusStop stop in busStops)
             {
-                BusStop randomStop = busStops[random.Next(busStops.Length)];
-                if (randomStop != person.CurrentStop)
-                {
-                    person.WantedStop = randomStop;
-                }
+                _totalWeighting += stop.Weighting;
             }
+        }
+
+        private void Start()
+        {
+            StartGame();
+        }
+
+        List<Person> people = new List<Person>();
+
+        public int GetTotalPeople()
+        {
+            return people.Count;
+        }
+
+        private void StartGame()
+        {
+            /*// TODO: Change this to be dynamically set
+            float NumberOfPassengersThisRound = 10;
+            foreach(BusStop busStop in busStops)
+            {
+                people.AddRange(busStop.GeneratePeople(Mathf.CeilToInt((busStop.Weighting / _totalWeighting) * NumberOfPassengersThisRound)));
+            }
+            System.Random random = new System.Random();
+            foreach(Person person in people)
+            {
+                while(person.WantedStop == null)
+                {
+                    BusStop randomStop = busStops[random.Next(busStops.Length)];
+                    if (randomStop != person.CurrentStop)
+                    {
+                        person.WantedStop = randomStop;
+                    }
+                }
+            }*/
+            SelectRandomStop();
+        }
+
+        /// <summary>
+        /// Select a random bus stop, taking into consideration the weighting the stop has been given
+        /// </summary>
+        /// <returns>A random bus stop</returns>
+        /// <exception cref="BusStopNotFoundException">Was unable to find a bus stop</exception>
+        private BusStop SelectRandomStop()
+        {
+            float[] chances = new float[busStops.Length];
+            float totalWeighting = 0;
+            for (int i = 0; i < chances.Length; i++)
+            {
+                chances[i] = busStops[i].Weighting;
+                totalWeighting += chances[i];
+            }
+            for (int i = 0; i < chances.Length; i++)
+            {
+                chances[i] /= totalWeighting;
+                if (i > 0) { chances[i] += chances[i - 1]; }; // makes chances cumulative
+            }
+
+            double random = Randomiser.NextDouble();
+
+            for (int i = chances.Length - 1; i >= 0; i--)
+            {
+                if (chances[i] > random) { return busStops[i]; };
+            }
+            throw new BusStopNotFoundException("Cannot find bus stop :(");
         }
     }
 }
