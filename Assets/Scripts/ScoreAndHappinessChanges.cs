e7c4e207869391adee34191074bde01f38d4a422
Logan King <65763483+loganator956@users.noreply.github.com>
2022-08-06 12:21:37 +0100

    Update ScoreAndHappinessChanges.cs

--- Assets/Scripts/ScoreAndHappinessChanges.cs
+++ Assets/Scripts/ScoreAndHappinessChanges.cs
@@ -5,5 +5,6 @@ using UnityEngine;
 public static class ScoreAndHappinessChanges
 {
     public const int PassengerWaitTooLongPenalty = -2;
+    public const int BusIdlePenalty = -2;
     public const int AwardPassengerUnload = 2;
 }
