a28ca20a6bbf8b378b3b25d2457547673cac7344
Logan King <65763483+loganator956@users.noreply.github.com>
2022-10-07 20:16:30 +0100

    Tidy code a bit

--- Assets/Scripts/Player/TopDownVehicleController.cs
+++ Assets/Scripts/Player/TopDownVehicleController.cs
@@ -8,25 +8,28 @@ using UnityEngine.InputSystem;
 public class TopDownVehicleController : MonoBehaviour
 {
     [Header("Vehicle Properties")]
-    public float DriftFactor = 0.55f;
-    public float AccelerationSpeed = 1f;
-    private float _decelerationInput = 0.995f;
-    public float MinDecelerateFactor = 0.995f;
-    public float MaxDecelerateFactor = 0.9f;
-    public AnimationCurve DecelerateCurve;
+    public float AccelerationSpeed = 30f;
     public float MaxTurnSpeed = 5.0f;
+    [Header("Vehicle Physics")]
+    [Range(0f, 1f)]
+    public float MinDecelerateFactor = 0.995f; // Decelerate Factor influences the amount of speed the player KEEPS. So a factor of 0.995f means that the player keeps 0.995f of their velocity each frame
+    [Range(0f, 1f)]
+    public float MaxDecelerateFactor = 0.9f;
+    public AnimationCurve DecelerateCurve; // Determines how the decelerate factor changes from min to max over time (evaluates the T value of lerp between min and max decelerate factors from time)
+    public float DriftFactor = 0.55f; // The amount of sideways velocity to keep (0.55 * right velocity)
 
+    // Unity Input System
     private PlayerInput _playerInput;
-    public PlayerInput PlayerInput
-    {
-        get { return _playerInput; }
-    }
     private InputAction _moveAction;
 
+    // Components
     private Rigidbody _rigidbody;
 
-    private float accelerationInput = 0f;
+    // Temp Input Values
+    private float _accelerationInput = 0f;
+    private float _decelerationInput = 0.995f;
 
+    // Constants
     private const float SteerAngleDeadzone = 0.05f;
 
     private void Awake()
@@ -55,9 +58,9 @@ public class TopDownVehicleController : MonoBehaviour
             _rigidbody.MoveRotation(Quaternion.Lerp(transform.rotation, rotation, MaxTurnSpeed / angle/*0.2f*/));
         }
 
-        accelerationInput = GetAccelerationFromInput(dirInput);
+        _accelerationInput = GetAccelerationFromInput(dirInput);
 
-        if (accelerationInput < 0.05f)
+        if (_accelerationInput < 0.05f)
         {
             // no inputs (slowdown)
             decelerateT += Time.deltaTime;
@@ -74,17 +77,17 @@ public class TopDownVehicleController : MonoBehaviour
     private void FixedUpdate()
     {
         ApplyEngineForce();
-        ReduceOrthogonalVelocity();
+        ApplyPhysicsForces();
     }
 
     private void ApplyEngineForce()
     {
-        Vector3 engineForceVector = transform.forward * accelerationInput * AccelerationSpeed;
+        Vector3 engineForceVector = transform.forward * _accelerationInput * AccelerationSpeed;
 
         _rigidbody.AddForce(engineForceVector, ForceMode.Force);
     }
 
-    private void ReduceOrthogonalVelocity()
+    private void ApplyPhysicsForces()
     {
         Vector3 forwardVelocity = transform.forward * Vector3.Dot(_rigidbody.velocity, transform.forward);
         Vector3 rightVelocity = transform.right * Vector3.Dot(_rigidbody.velocity, transform.right);
@@ -95,7 +98,7 @@ public class TopDownVehicleController : MonoBehaviour
     private Quaternion GetRotationFromInput(Vector2 input)
     {
         float angle = Vector2.SignedAngle(Vector3.up, input);
-        return Quaternion.Euler(0, -angle, 0);
+        return Quaternion.Euler(0, -angle + 45f, 0);
     }
 
     private float GetAccelerationFromInput(Vector2 input)
